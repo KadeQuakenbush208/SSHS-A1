145669ab24bcfdd929de77cf79964514ea4ca019
StudentLW1 <lhw184@student.bham.ac.uk>
2025-11-01 17:02:52 +0000

    fixed crash complaint

--- fuzz/run.sh
+++ fuzz/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t sshs_asg1 -f Dockerfile .
-winpty docker run --name asg1 -v .:/workspace -it sshs_asg1 afl-fuzz -i in -o out -- ./js_afl @@
+winpty docker run --privileged --name asg1 -v .:/workspace -it sshs_asg1 sh -c "echo core > /proc/sys/kernel/core_pattern && afl-fuzz -i in -o out -- ./js_afl @@"
