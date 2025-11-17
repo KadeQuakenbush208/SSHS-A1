e3e13245eba61f20ede86d747ed042877d1806ce
Kade Quakenbush <jedimasterkade@gmail.com>
2025-11-17 12:53:50 +0000

    Changed Dockerfile to install libraries, patch the source file, compile the original and patched programs with AddressSanitizer.
    Changed run.sh to run the binaries for the original and patched programs using the input used in crash ID 000000, also added runtime annotations.

--- fixed/run.sh
+++ fixed/run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+docker rm -f asg2 2>/dev/null || true
 docker build -t sshs_asg2 -f Dockerfile .
-docker run -v .:/workspace -it sshs_asg2
+docker run --privileged --name asg2 -v .:/workspace -it sshs_asg2 sh -c "
+	echo 'Running original program with input crashes/id:000000...' &&
+	./js_asan crashes/id:000000,sig:11,src:000001,time:48164,execs:159600,op:havoc,rep:2 ;
+	echo 'Running patched program with input crashes/id:000000...' &&
+	./js-patched_asan crashes/id:000000,sig:11,src:000001,time:48164,execs:159600,op:havoc,rep:2
+"
