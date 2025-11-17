4b18e14dfa6b08cc91d74dae407467583df09950
Kade Quakenbush <jedimasterkade@gmail.com>
2025-11-17 13:12:37 +0000

    Moved crashes folder into in. Adjusted run.sh accordingly

--- fixed/run.sh
+++ fixed/run.sh
@@ -3,7 +3,7 @@
 docker build -t sshs_asg2 -f Dockerfile .
 docker run --privileged --name asg2 -v .:/workspace -it sshs_asg2 sh -c "
 	echo 'Running original program with input crashes/id:000000...' &&
-	./js_asan crashes/id:000000,sig:11,src:000001,time:48164,execs:159600,op:havoc,rep:2 ;
+	./js_asan in/crashes/id:000000,sig:11,src:000001,time:48164,execs:159600,op:havoc,rep:2 ;
 	echo 'Running patched program with input crashes/id:000000...' &&
-	./js-patched_asan crashes/id:000000,sig:11,src:000001,time:48164,execs:159600,op:havoc,rep:2
+	./js-patched_asan in/crashes/id:000000,sig:11,src:000001,time:48164,execs:159600,op:havoc,rep:2
 "
